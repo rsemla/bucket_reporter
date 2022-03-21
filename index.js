230a55d7dfea6ecb14b727a51232e61fda96f0d0
Ryan Semla <rsemla@iu.edu>
2022-03-21 00:57:43 +0000

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const {Storage} = require('@google-cloud/storage');
+const fs = require('fs');
+var countFiles = require('count-files')
 
 const storage = new Storage();
 
-const targetBucket = storage.bucket('iupui-cit41200-class-malpdf-pdf-sort');
+const maliciousBucket = storage.bucket('sp22-cit41200-class-malpdf-pdf-malicious');
+const benignBucket = storage.bucket('sp22-cit41200-class-malpdf-pdf-benign');
 
-async function deleteFiles () {
-  await targetBucket.deleteFiles(function(err) {
-    console.log(`All files in ${targetBucket} deleted.`);
+exports.readBuckets = (file, context) => {
+
+    async function scanMaliciousFiles () {
+        const [files] = await maliciousBucket.getFiles();
+        
+        //var stats = countFiles(maliciousBucket, function (err, results) {
+         //   console.log('done counting')
+         //   console.log(results) // { files: 10, dirs: 2, bytes: 234 }
+         // });
+
+       // fs.readdir( 'https://console.cloud.google.com/storage/browser/sp22-cit41200-class-malpdf-pdf-malicious', (error, files) => { 
+       files.forEach(file => {
+        //    countFiles = sum(file);
+        //console.count(`Malicous files: ${file.name}`);
+        console.count("Malicious Files ");
+       //console.log("Malicious Files: "+countFiles);
   });
-  
+};
+async function scanBenignFiles () {
+    const [files] = await benignBucket.getFiles();
+    
+    var countFiles = require('count-files')
+ 
+   // var stats = countFiles(benignBucket, function (err, results) {
+     // console.log('done counting')
+     // console.log(results) // { files: 10, dirs: 2, bytes: 234 }
+   // });
+
+    files.forEach(file => {
+        //countFiles = sum(file);
+   // console.count(`Benign files: ${file.name}`);
+   console.count("Benign Files ");
+    //console.log("Benign Files: "+countFiles);
+});
 };
 
-deleteFiles();
+scanMaliciousFiles();
+scanBenignFiles();
+
+};
